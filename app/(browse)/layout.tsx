cd2c13a35d449ae6b9bd633979dc64d5fd929989
MrCrafter32 <jagadeesh.d125@gmail.com>
2024-01-04 20:34:02 +0530

    added analytics and speed insights

--- app/(browse)/layout.tsx
+++ app/(browse)/layout.tsx
@@ -2,6 +2,9 @@ import { Sidebar, SidebarSkeleton } from "./_components/sidebar/page";
 import { Navbar } from "./_components/navbar/page";
 import { Container } from "./_components/container";
 import { Suspense } from "react";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+
 
 const browseLayout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -11,7 +14,12 @@ const browseLayout = ({ children }: { children: React.ReactNode }) => {
         <Suspense fallback={<SidebarSkeleton />}>
         <Sidebar />
         </Suspense>
-        <Container>{children}</Container>
+        <Container>
+          {children}
+          <Analytics />
+          <SpeedInsights />
+
+        </Container>
       </div>
     </>
   );
