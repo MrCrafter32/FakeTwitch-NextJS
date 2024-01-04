a2181f4818c7acb7b308e135688b9f48725c4bd5
MrCrafter32 <jagadeesh.d125@gmail.com>
2024-01-04 19:41:51 +0530

    fixed build issues

--- app/(browse)/(home)/_components/result-card.tsx
+++ app/(browse)/(home)/_components/result-card.tsx
@@ -3,7 +3,7 @@ import { Stream, User } from "@prisma/client";
 
 import { Thumbnail, ThumbnailSkeleton } from "@/components/thumbnail";
 import { Skeleton } from "@/components/ui/skeleton";
-import { LiveBadge } from "@/components/live-badge";
+import { LiveBadge } from "@/components/livebadge";
 import { UserAvatar, UserAvatarSkeleton } from "@/components/user-avatar";
 
 interface ResultCardProps {
